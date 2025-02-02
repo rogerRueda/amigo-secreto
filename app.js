e506f83f63c5ab015bb0f9f9f8f16144b6bc5caf
rogerRueda <r.rueda0199@gmail.com>
2025-02-01 20:13:10 -0600

    Agregamos alerta para validar que el campo no este vacío

--- app.js
+++ app.js
@@ -4,4 +4,8 @@ let amigos = [];
 //Agregamos una funcion para agregar los nombres de los amigos
 function agregarAmigos () {
     let nombreAmigo = document.getElementById("amigo").value;
+    //alerta para validar que el campo no este vacío
+    if (nombreAmigo.trim() === "") {
+        alert("Por favor, inserte un nombre");
+    }
 }
