59892a52e679db3f2f7dacc08abfec0e7abac629
rogerRueda <r.rueda0199@gmail.com>
2025-02-01 20:08:53 -0600

    Agregamos la funcion para agregar los nombres

--- app.js
+++ app.js
@@ -1,2 +1,7 @@
 //Creamos el array para agregar los nombres de amigos
 let amigos = [];
+
+//Agregamos una funcion para agregar los nombres de los amigos
+function agregarAmigos () {
+    let nombreAmigo = document.getElementById("amigo").value;
+}
