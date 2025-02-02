d11f221b357c91f75840b8522bb9dece0ec1a44d
rogerRueda <r.rueda0199@gmail.com>
2025-02-01 20:23:19 -0600

    Actualizamos funcion para agregar nombre de los amigos

--- app.js
+++ app.js
@@ -2,10 +2,14 @@
 let amigos = [];
 
 //Agregamos una funcion para agregar los nombres de los amigos
-function agregarAmigos () {
+function agregarAmigo() {
     let nombreAmigo = document.getElementById("amigo").value;
     //alerta para validar que el campo no este vacío
     if (nombreAmigo.trim() === "") {
         alert("Por favor, inserte un nombre");
+    } else {
+        amigos.push(nombreAmigo);
+        document.querySelector("#amigo").value = "";
+        mostrarListaAmigo();
     }
 }
